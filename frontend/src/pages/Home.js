c1642ddd25eba9040f5207e5f9f63df033fdb7b6
oohhgabe <98634635+oohhgabe@users.noreply.github.com>
2022-11-13 19:07:50 -0600

    Create home page, fix color scheme

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
-import img1 from './images/SE.jpg';
-function Home() {
-  return (
-    <div>
-      <img src={img1} alt=""/>
-    </div>
+import { useNavigate } from "react-router-dom";
+import HomeImage from "../assets/HomeImage.jpg";
+import "./Home.css";
 
-  )
+function Home() {
+  let navigate = useNavigate();
+
+  const goToReserve = async (event) => {
+    event.preventDefault();
+    navigate("/reserve");
+  };
+
+  const goToRegister = async (event) => {
+    event.preventDefault();
+    navigate("/register");
+  };
+  return (
+    <div className="home">
+      <img src={HomeImage} alt="backgroundImage" />
+      <button onClick={goToReserve} className="reservation">
+        Reserve a Table
+      </button>
+      <button onClick={goToRegister} className="register">
+        Create an account
+      </button>
+    </div>
+  );
 }
 
 export default Home;
