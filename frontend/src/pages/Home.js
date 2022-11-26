0ca6f5f0fd72708e04f522eba8bb1ed4d8026b01
Nick Hiller <nahiller07@gmail.com>
2022-11-26 13:23:09 -0600

    added reserve button to home and changed hover affecs

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import HomeImage from "../assets/HomeImage.jpg";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  let navigate = useNavigate();
 
+  const goToReserve = async (event) => {
+    event.preventDefault();
+    navigate("/reserve");
+  };
   return (
     <div className="home">
       <img src={HomeImage} alt="backgroundImage" />
       <div class="centered">La Habibi</div>
+      <input type="submit" className="homebutton" value="Reserve" onClick={goToReserve}></input>
+
     </div>
   );
 }
